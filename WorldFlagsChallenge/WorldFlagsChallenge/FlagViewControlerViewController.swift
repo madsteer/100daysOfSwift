4acb1503b5fe5333a685c05a4bc87e5e63d337c4
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-10-05 12:35:58 -0500

    day23 challenge: tighten up code around optional selectedFlag and add ability to share image

--- WorldFlagsChallenge/WorldFlagsChallenge/FlagViewControlerViewController.swift
+++ WorldFlagsChallenge/WorldFlagsChallenge/FlagViewControlerViewController.swift
@@ -15,7 +15,21 @@ class FlagViewControlerViewController: UIViewController {
         super.viewDidLoad()
 
         title = selectedFlag!.uppercased()
-        flag.image = UIImage(named: selectedFlag!)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareFlag))
+        
+        if let imageToLoad = selectedFlag {
+            flag.image = UIImage(named: imageToLoad)
+        }
     }
     
+    @objc func shareFlag() {
+        guard let image = flag.image?.jpegData(compressionQuality: 0.8) else {
+            print("no image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image, selectedFlag!.uppercased()], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
 }
