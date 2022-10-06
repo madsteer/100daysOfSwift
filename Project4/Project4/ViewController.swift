8b3331f98e71d4a6ba59e4f935427a33763654cc
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-10-06 09:27:20 -0500

    project 4: initial setup

---
+++ Project4/Project4/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Cory Steers on 10/6/22.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://www.hackingwithswift.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+
+}
+
