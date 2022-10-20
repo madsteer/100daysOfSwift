32917b8e325e8018a5f8e2ba4b97dbf2b02e8b1f
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-10-20 13:24:55 -0500

    project 9: copy project 7 to project 9 and move internet data retrieval and json processing to a background thread

---
+++ Project9/Project7/ViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Cory Steers on 10/18/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let add = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterResults))
+        let credits = UIBarButtonItem(title: "Credits",style: .plain, target: self, action: #selector(showCredits))
+        navigationItem.rightBarButtonItem = add
+        navigationItem.leftBarButtonItem = credits
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+//            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+//            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            if let url = URL(string: urlString) {
+                if let data = try? Data(contentsOf: url) {
+                    self?.parse(json: data)
+                    return
+                }
+            }
+            self?.showError()
+        }
+        
+    }
+
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+            petitions = jsonPetitions.results
+            filteredPetitions = petitions
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.reloadData()
+            }
+        }
+    }
+
+    func showError() {
+        DispatchQueue.main.async { [weak self] in
+            let ac  = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self?.present(ac, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredPetitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = filteredPetitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc func showCredits() {
+        let ac = UIAlertController(title: "Credits", message: "We The People API of the Whitehouse", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func filterResults() {
+        let ac = UIAlertController(title: "Filter results", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Filter", style: .default) { [weak self, weak ac] _ in
+            guard let text = ac?.textFields?[0].text else { return }
+            self?.submit(text)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        if item.isEmpty {
+            filteredPetitions = petitions
+        } else {
+            filteredPetitions = petitions.filter { petition in
+                return petition.title.lowercased().contains(item.lowercased())
+            }
+        }
+        tableView.reloadData()
+    }
+}
+
