df0c7eb8f91ec88633d19d87ad5f99f3617577f0
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-10-24 16:13:23 -0500

    project 10: finish tutorial

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Cory Steers on 10/24/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
