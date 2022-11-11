83ebb4e6c8478fc8645a82da64a8e1bc60ec24a4
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-11-11 15:24:41 -0600

    fix name of challenge project

---
+++ PhotoCaption/PhotoCaption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoCaption
+//
+//  Created by Cory Steers on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
