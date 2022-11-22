474077d8d8e07f6ddbbd79d2a8ed4595b7e261c5
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-11-22 15:24:51 -0600

    project 13: initial creation and UI design

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Cory Steers on 11/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+    }
+}
+
