d98d747b6036647913dbc9b5973a37eb2968d41a
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-12-08 15:01:29 -0600

    project 15: initial setup

---
+++ Project15/Project15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Cory Steers on 12/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
