b274e37e42f8770d8de9dab8d25f720c5e74bbe2
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-12-12 14:05:53 -0600

    project 17: initial setup

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Cory Steers on 12/12/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
