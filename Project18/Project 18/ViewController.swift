963e7a62a5fa8a5f7f6653044d7a59fd54af3d4a
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-12-19 15:19:32 -0600

    renamed project folder

---
+++ Project18/Project 18/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 18
+//
+//  Created by Cory Steers on 12/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i).")
+        }
+    }
+}
+
