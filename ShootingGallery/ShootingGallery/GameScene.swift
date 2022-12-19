f182371392c60ae1a9969c635d86664fee84ba53
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-12-19 15:46:02 -0600

    day 66 milestone challenge: initial setup

---
+++ ShootingGallery/ShootingGallery/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  ShootingGallery
+//
+//  Created by Cory Steers on 12/19/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+
+}
