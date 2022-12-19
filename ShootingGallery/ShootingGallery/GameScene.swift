723886531a5f6e4b991e24abdd28ed108b22aee6
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-12-19 16:23:30 -0600

    day 66 milestone challenge: set background and present score

--- ShootingGallery/ShootingGallery/GameScene.swift
+++ ShootingGallery/ShootingGallery/GameScene.swift
@@ -6,13 +6,33 @@
 //
 
 import SpriteKit
-import GameplayKit
 
 class GameScene: SKScene {
+    var scoreLabel: SKLabelNode!
+    
+    var possibleTargets = [ "target0", "target1", "target2", "target3" ]
+    var gameTimer: Timer?
+    
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score \(score)"
+        }
+    }
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "wood-background")
+        background.position = CGPoint(x: frame.size.width / 2, y: frame.size.height / 2)
+        background.size = CGSize(width: self.size.width, height: self.size.height)
+        background.blendMode = .replace
+        background.zPosition = -2
+        addChild(background)
         
-        
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.position = CGPoint(x: 130, y: 50)
+        scoreLabel.fontSize = 48
+        scoreLabel.zPosition = -1
+        addChild(scoreLabel)
     }
     
 
