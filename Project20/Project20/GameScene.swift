e3f6fda6b179948ef66fa1ad7cb992cbd19a6cfa
Cory Steers <cory.steers.gmu9@statefarm.com>
2022-12-22 15:26:48 -0600

    project 20: initial setup

---
+++ Project20/Project20/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Cory Steers on 12/22/22.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
