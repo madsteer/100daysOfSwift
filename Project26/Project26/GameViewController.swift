04b5c0462ec601630b8b10fc02be7fea6ba63d35
Cory Steers <developer@madsteer.com>
2023-12-13 16:05:20 -0600

    project26: Loading a level: categoryBitMask, collisionBitMask, contactTestBitMask

--- Project26/Project26/GameViewController.swift
+++ Project26/Project26/GameViewController.swift
@@ -1,11 +1,10 @@
 //
 //  GameViewController.swift
-//  ShootingGallery
+//  Project26
 //
-//  Created by Cory Steers on 12/19/22.
+//  Created by Cory Steers on 12/12/23.
 //
 
-import AVFoundation
 import UIKit
 import SpriteKit
 import GameplayKit
@@ -14,33 +13,24 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-        } catch  {
-            print("could not set session. err:\(error.localizedDescription)")
-        }
-        do {
-            try AVAudioSession.sharedInstance().setActive(true)
-        } catch {
-            print("could not active session. err:\(error.localizedDescription)")
-        }
-        
+
+        // swiftlint: disable force_cast
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
+
                 // Present the scene
                 view.presentScene(scene)
             }
-            
+
             view.ignoresSiblingOrder = true
-            
+
             view.showsFPS = true
             view.showsNodeCount = true
         }
+        // swiftlint: enable force_cast
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
